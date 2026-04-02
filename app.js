ff6c147490e50aa32885cecf3dbed8272a05d32e
joaolucasraday <lucasraday@gmail.com>
2026-04-02 17:14:04 -0300

    vercel fix error 500

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
-});
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname));
-
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-
-app.listen(3000, ()=>{
-    console.log(`Servidor rodando em http://localhost:${3000}`);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
+
+module.exports = app;
