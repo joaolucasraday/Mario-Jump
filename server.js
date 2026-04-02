e7f4d7ae7d9f30fd62f5a8b13b310b2b8507b8ec
joaolucasraday <lucasraday@gmail.com>
2026-04-02 17:52:38 -0300

    fix local server

--- server.js
+++ server.js
@@ -3,12 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.json());
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'home.html'));
+app.listen(3000, () => {
+  console.log('Rodando em http://localhost:3000');
 });
-
-module.exports = app;
