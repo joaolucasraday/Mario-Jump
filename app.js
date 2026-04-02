5dbd3efb5a3562abc5c02a0c6e5cc4e4390f727f
joaolucasraday <lucasraday@gmail.com>
2026-04-02 10:59:50 -0300

    not bd

--- app.js
+++ app.js
@@ -12,36 +12,11 @@ app.get('/', (req, res) => {
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(__dirname));
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-
 app.use(express.json());
 
-app.post('/api/auth/signup', async (req, res) => {
-    const { username, password } = req.body;
-    const fakeEmail = `${username.toLowerCase()}@mariojump.com`;
 
-    const { data, error } = await supabase.auth.signUp({
-        email: fakeEmail,
-        password: password,
-        options: { data: { display_name: username } }
-    });
 
-    if (error) return res.status(400).json({ error: error.message });
-    res.json({ message: "Usuário criado", user: data.user });
-});
 
-app.post('/api/auth/login', async (req, res) => {
-    const { username, password } = req.body;
-    const fakeEmail = `${username.toLowerCase()}@mariojump.com`;
-
-    const { data, error } = await supabase.auth.signInWithPassword({
-        email: fakeEmail,
-        password: password,
-    });
-
-    if (error) return res.status(400).json({ error: "Usuário ou senha incorretos" });
-    res.json({ message: "Logado", user: data.user });
-});
 
 app.listen(3000, ()=>{
     console.log(`Servidor rodando em http://localhost:${3000}`);
--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
-const loginContainer = document.querySelector('.loginContainer')
 let score = 0;
 let canScore = true;
 let isJumping = false;
@@ -56,14 +55,6 @@ const loop = setInterval(() => {
         canScore = true;
     }
 }, 10);
-
-function loginUsuario() {
-    loginContainer.style.display = 'block';
-}
-
-function fecharContainer() {
-    loginContainer.style.display = 'none';
-}
 
 document.addEventListener('keydown', (e) => {
     if (e.code === 'Space' || e.code === 'ArrowUp' || e.code === 'KeyW') {
